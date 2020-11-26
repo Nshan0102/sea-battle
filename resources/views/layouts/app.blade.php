7ca46933601d3211e055289e76bef0227156c560
Mher Shahinyan <shahinyanm1@gmail.com>
2020-11-26 13:01:46 +0400

    metadata added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:title" content="Armenian Sea Battle" />
+    <meta property="og:description" content="Join to Sea Battle and have fun with your friends" />
+    <meta property="og:image" content="{{ asset('favicon.png') }}" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
