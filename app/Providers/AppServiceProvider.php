b69d98ff571e75aa3aa56f17e9111a96183fc71a
Nshan <Nshan.Vardanyan.1996@@gmail.com>
2020-12-21 21:55:44 +0400

    make routing to force ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if (env('APP_ENV') !== 'local') {
+        if ($this->app->environment('production')) {
             $url->forceScheme('https');
         }
     }
