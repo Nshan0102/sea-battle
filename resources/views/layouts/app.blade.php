e81d17e5e787e7cb8d210d71ffa5e50b4469cac0
Anahit Avetisyan <anahitavetisyan1993@gmail.com>
2021-01-13 18:18:55 +0400

    add new style for header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,56 +32,60 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
+            <div class="collapse navbar-collapse" id="navbarText">
+                <div class="navbar-nav-container">
+                    <ul class="navbar-nav">
+                        <!-- Authentication Links -->
+                        @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
+                        @endguest
+                    </ul>
+                    @if(Auth::user())
+                        <ul class="navbar-nav  ">
+                            <li class="nav-item">
+                                <i class="fa fa-envelope icon"></i>
+                                <a class="nav-link" >Messages</a>
+                            </li>
+                            <li class="nav-item">
+                                <i class="fa fa-chalkboard-teacher icon"></i>
+                                <a class="nav-link" >Instructions</a>
+                            </li>
+                            <li class="nav-item">
+                                <i class="fas fa-users icon"></i>
+                                <a class="nav-link" >Sea Battle Team</a>
+                            </li>
+                            <li class="nav-item">
+                                <i class="fas fa-sign-out-alt icon"></i>
+                                <a class="nav-link" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
-
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
+                            </li>
+                        </ul>
+                    @endif
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
     @yield('content')
 </div>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"
